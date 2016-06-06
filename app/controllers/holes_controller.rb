e31aa761cd3537759871b1fd1b4127a9ac23bb59
melissajimison <melissajimison@gmail.com>
2016-06-05 20:07:50 -0700

    started the enviroment for the model Holes

--- app/controllers/holes_controller.rb
+++ app/controllers/holes_controller.rb
@@ -1,6 +1,33 @@
 require "#{Rails.root}/lib/Geocode.rb"
 class HolesController < ApplicationController
+  def index
+
+  end
+  def new
+    @hole = Hole.new
+  end
+
+  def create
+    @hole = Hole.new
+    @hole.current_user_id = params[:current_user_id]
+    @hole.origin_latitud = params[:origin_latitud]
+    @hole.origin_longitud = params[:origin_longitud]
+    @hole.origin_image =params[:origin_image]
+    @hole.destination_latitud = params[:destination_latitud]
+    @hole.destination_longitud = params[:destination_longitud]
+    @hole.destination_image = params[:destination_image]
+    @hole.save!
+    if @hole.save
+      redirect_to hole_path(@hole.current_user_id)
+    else
+      render :index
+    end
+  end
+
   def show
+  end
+
+  def destroy
   end
 
 
@@ -12,11 +39,17 @@ class HolesController < ApplicationController
       @location = data
       @user_input = params[:user_input]
       @destination_latitud = @location.latitud
-      @destination_longitud = @location.longitud.abs
+      @destination_longitud = @location.longitud * (-1)
       @destination = Geocode.find_destination(@destination_latitud,@destination_longitud)
-      
-      render :show
+
+      render :index
     end
 
+  end
+
+  private
+
+  def hole_create_params
+    params.require(:hole).permit(:current_user_id, :origin_latitud, :origin_longitud, :origin_image, :destination_latitud, :destination_longitud, :destination_image)
   end
 end
